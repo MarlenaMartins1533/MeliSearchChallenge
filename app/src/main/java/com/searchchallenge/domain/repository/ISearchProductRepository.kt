2f82dac499769dd76d80d070793138ec7379abda
MarlenaMartins1533 <marlenamotamartins@gmail.com>
2025-04-08 21:58:02 -0300

    Domain. Adjustments domain, val inserted.

--- app/src/main/java/com/searchchallenge/domain/repository/ISearchProductRepository.kt
+++ app/src/main/java/com/searchchallenge/domain/repository/ISearchProductRepository.kt
@@ -2,6 +2,6 @@ package com.searchchallenge.domain.repository
 
 import com.searchchallenge.ui.composable.model.Product
 
-interface SearchProductRepository {
+interface ISearchProductRepository {
     suspend fun searchProducts(query: String): List<Product>
 }
