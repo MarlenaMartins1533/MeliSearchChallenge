3d24245f8cab8221ccc1e1973ecaa502e506917d
MarlenaMartins1533 <marlenamotamartins@gmail.com>
2025-04-09 15:43:58 -0300

    Data.SearchFun

--- app/src/main/java/com/searchchallenge/ui/main/MainActivity.kt
+++ app/src/main/java/com/searchchallenge/ui/main/MainActivity.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.searchchallenge.ui.composable.ProductListScreen
 import com.searchchallenge.ui.composable.SearchScreen
 import com.searchchallenge.ui.composable.parameterprovider.productListNamesParameterProvider
-import com.searchchallenge.ui.composable.parameterprovider.productListParameterProvider
 import com.searchchallenge.ui.theme.SearchChallengeTheme
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -26,7 +33,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             SearchChallengeTheme {
                 SearchProducts()
-//            }
             }
         }
     }
@@ -34,19 +40,20 @@ class MainActivity : ComponentActivity() {
     @Preview(showBackground = true)
     @Composable
     fun SearchProducts() {
-        val searchResults = remember { mutableStateOf(productListNamesParameterProvider.first()) }
+        var searchResults by rememberSaveable { mutableStateOf(productListNamesParameterProvider.first()) }
 
         SearchScreen(
             onSearch = { query ->
-                searchResults.value = "Search result for: $query"
+                searchResults = "Search result for: $query"
                 // Your search logic here.  For now, let's just update the searchResults:
                 //chama fun da VM e traz os resultados da busca do Meli
                 mainViewModel.searchProducts(query)
             }
         ) {
             Column(
+                modifier = Modifier.fillMaxSize().padding(it).background(Color.LightGray),
             ) {
-                Text(searchResults.value)
+                Text(searchResults)
                 ProductListScreen(productItems = mainViewModel.searchProductData.value)
             }
         }
