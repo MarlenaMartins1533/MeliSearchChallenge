42a82a96ee8d3388476278603149bbe9e6f86b42
MarlenaMartins1533 <marlenamotamartins@gmail.com>
2025-04-08 21:21:38 -0300

    Domain. MainViewModel fun, searchProductData.

--- app/src/main/java/com/searchchallenge/ui/main/MainViewModel.kt
+++ app/src/main/java/com/searchchallenge/ui/main/MainViewModel.kt
@@ -1,5 +1,27 @@
 package com.searchchallenge.ui.main
 
-class MainViewModel {
-    fun searchProducts(query: String) {}
+import android.util.Log
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.searchchallenge.ui.composable.model.Product
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+
+class MainViewModel(
+) : ViewModel() {
+
+    private val _searchProductData = mutableStateOf<List<Product>>(emptyList())
+    val searchProductData: State<List<Product>> = _searchProductData
+
+    fun searchProducts(query: String) {
+        viewModelScope.launch {
+            try {
+
+            } catch (e: HttpException) {
+                Log.e("error", e.message ?: "ERROR")
+            }
+        }
+    }
 }
