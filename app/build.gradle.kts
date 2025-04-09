2c9e96446369783b1d5816d3a7dd7019306ea70f
MarlenaMartins1533 <marlenamotamartins@gmail.com>
2025-04-08 21:21:38 -0300

    Domain. MainViewModel fun, searchProductData.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation(libs.androidx.material)
     implementation(libs.androidx.material3)
     implementation(libs.koin.core)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
