8a4e50750e914aea16070a5bdf411d118be2c465
MarlenaMartins1533 <marlenamotamartins@gmail.com>
2025-04-10 21:12:53 -0300

    Unit Tests created

---
+++ app/src/test/java/com/searchchallenge/ProductResourcesProvider.kt
@@ -0,0 +1,55 @@
+package com.searchchallenge
+
+import com.searchchallenge.domain.model.PictureDomain
+import com.searchchallenge.domain.model.ProductDomain
+import com.searchchallenge.ui.composable.model.Product
+
+// Mock data
+
+object Resources {
+    const val Query = "s20"
+
+    var productDomainListParameterProvider = mutableListOf(
+        ProductDomain(
+            productId = "Sample Id",
+            productName = "Sample Name",
+            pictures = mutableListOf(
+                PictureDomain(
+                    " ",
+                    "R.drawable.baseline_add_shopping_cart_24"
+                )
+            ),
+            description = "Sample description! " +
+                    "Produto mirabilis ad [activitatem] cum [pluma desiderata] et [beneficium clavem].",
+        ),
+        ProductDomain(
+            productId = "Sample Id",
+            productName = "Sample Second Name",
+            pictures = mutableListOf(
+                PictureDomain(
+                    " ",
+                    "R.drawable.baseline_add_shopping_cart_24"
+                )
+            ),
+            description = "Sample second description! " +
+                    "Produto Second mirabilis ad [activitatem] cum [pluma desiderata] et [beneficium clavem].",
+        )
+    )
+
+    var productListParameterProvider = listOf(
+        Product(
+            id = "Sample Id",
+            name = "Sample Name",
+            images = listOf("R.drawable.baseline_add_shopping_cart_24"),
+            description = "Sample description! " +
+                    "Produto mirabilis ad [activitatem] cum [pluma desiderata] et [beneficium clavem].",
+        ),
+        Product(
+            id = "Sample Id",
+            name = "Sample Second Name",
+            images = listOf("R.drawable.baseline_add_shopping_cart_24"),
+            description = "Sample second description! " +
+                    "Produto Second mirabilis ad [activitatem] cum [pluma desiderata] et [beneficium clavem].",
+        )
+    )
+}
